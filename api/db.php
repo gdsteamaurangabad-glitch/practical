4ad4b12e1d4ad11cf632eae5c13a31fe41e8acd1
gdsteamaurangabad-glitch <gdsteamaurangabad@gmail.com>
2026-07-27 20:27:50 +0530

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,4 +1,14 @@
-options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+<?php
+$host   = getenv('DB_HOST');
+$user   = getenv('DB_USER');
+$pass   = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$port   = getenv('DB_PORT');
+
+$conn = mysqli_init();
+
+// Required SSL settings for Aiven
+$conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 $conn->ssl_set(NULL, NULL, NULL, NULL, NULL);
 
 $success = $conn->real_connect($host, $user, $pass, $dbname, (int)$port, NULL, MYSQLI_CLIENT_SSL);
